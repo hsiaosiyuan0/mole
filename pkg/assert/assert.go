6abbef5978aecca1ac24300c9da0d569967d62b3
hsiaosiyuan0 <hsiaosiyuan0@gmail.com>
2021-12-07 23:31:32 +0800

    chore: clean useless code

--- pkg/assert/assert.go
+++ pkg/assert/assert.go
@@ -1,9 +1,6 @@
 package assert
 
 import (
-	"fmt"
-	"os"
-	"os/exec"
 	"reflect"
 	"testing"
 
@@ -13,32 +10,6 @@ import (
 func Equal(t *testing.T, except, actual interface{}, msg string) {
 	if !reflect.DeepEqual(except, actual) {
 		t.Fatalf("%s Except: \n%v\nActual: \n%v", msg, except, actual)
-	}
-}
-
-var cyan = "\033[0;36m"
-var clear = "\033[0m"
-
-func EqualString(t *testing.T, except, actual string, msg string) {
-	if !reflect.DeepEqual(except, actual) {
-		fmt.Printf("%sActual:%s\n%s\n", cyan, clear, actual)
-		cmd := exec.Command("bash", "-c", fmt.Sprintf(`
-except=$(cat <<END
-%s
-END
-)
-
-actual=$(cat <<END
-%s
-END
-)
-
-diff --color=always -u <(echo "$except") <(echo "$actual")`, except, actual))
-		cmd.Stdin = os.Stdin
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		cmd.Run()
-		t.Fatalf("Failed due to strings not equal")
 	}
 }
 
