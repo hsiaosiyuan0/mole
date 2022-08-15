14f077b36c195d5c0cd2843ffbb06e5951f1ae38
hsiaosiyuan0 <hsiaosiyuan0@gmail.com>
2022-08-15 17:36:48 +0800

    fix: local path

--- cli/pack_test.go
+++ cli/pack_test.go
@@ -8,7 +8,7 @@ func TestPkgAna(t *testing.T) {
 	ana := &PkgAnalysis{}
 	opts := &Options{
 		packAna: true,
-		dir:     "/Users/hsiao/Developer/work/mole-tests/samples-data/web/st-podcast-social",
+		dir:     "",
 	}
 
 	ana.Process(opts)
